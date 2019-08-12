252ad893b8847e017681c0bbf8f1d3943217dec4
Elyorbek Soliev <elyorbeksoliev@gmail.com>
2019-08-11 21:44:36 -0400

    Updated

--- Links.py
+++ Links.py
@@ -6,6 +6,8 @@ import time
 
 
 
+
+
 #---------DECLARING VARIABLES -------------
 
 
@@ -27,7 +29,7 @@ driver = webdriver.Chrome(executable_path= chromePath)
 driver.delete_all_cookies()
 
 #---DECLARE IMPLICIT WAIT FOR ALL OBJECT---
-driver.implicitly_wait(5)
+driver.implicitly_wait(10)
 
 #---MAXIMIZE THE WINDOW ---
 driver.maximize_window()
@@ -40,16 +42,36 @@ driver.get(URL)
 
 #-----WORKING WITH LINKS -----
 
-links = driver.find_elements(By.TAG_NAME, "a")
+Links = driver.find_elements(By.TAG_NAME, "a")
 
-NumberOfLinks = len(links)
+NumberOfLinks = len(Links)
 
 print("Number of Links present:", NumberOfLinks)  # output:  Number of Links present: 481
 
 
+# ---So How we can Read the all the links? - for that we'll use For Loop Concept ---
+
+for link in Links:
+
+    print(link.text)
 
 
+# --- How to Click Particular Link? -
 
+
+driver.find_element(By.LINK_TEXT, "Flights").click()
+
+time.sleep(3)
+
+driver.back()
+
+time.sleep(1)
+
+
+driver.find_element(By.ID, "tab-cruise-tab-hp").click()
+
+
+driver.find_element(By.PARTIAL_LINK_TEXT, "Hot").click()
 
 
 driver.quit()
