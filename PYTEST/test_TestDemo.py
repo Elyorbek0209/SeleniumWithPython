2b42863022dd5408ab5ee8c4e5a1779c5424c788
Elyorbek Soliev <elyorbeksoliev@gmail.com>
2019-08-27 07:58:06 -0400

    Updated

--- PYTEST/test_TestDemo.py
+++ PYTEST/test_TestDemo.py
@@ -1,6 +1,15 @@
 import pytest
 
 
+#_------------PYTEST FRAMEWORK-------------
+
+# The Pytest Framework makes it easy to write
+# Small Tests Implemented on Top of our UnitTest
+
+
+
+#----NOTE ---> "PYTEST" Method should always with "test" word added ------
+
 def testMethod1():
 
     print("This is Test Method 1")
@@ -10,6 +19,24 @@ def testMethod2():
     print("This is Test Method 2")
 
 
+def testMethod3():
+
+    print("This is Test Method 3")
 
 
+def testMethod4():
+    print("This is Test Method 4")
+
+#------------------------------------------------------------------
+
+#---------So to run this above "PYTEST" Method we use Terminal----
+
+# How to open Terminal? - Got to "View" -> "Tool Windows" & Terminal
+
+# To run the test we use "pytest" or "pytest -v -s" command in Terminal
+
+# If we have multiple test in our package, we should specify test name
+# like "pytest -v -s test_TestDemo.py" otherwise it will executed the test of the test cases
+
+#-------------------------------------------------------------------
 
